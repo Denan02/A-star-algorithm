57efbd50f8572ad97e9db941470704d2692a3e04
denan <dpoturak1@etf.unsa.ba>
2025-11-05 23:37:36 +0100

    bug fix

--- main.cpp
+++ main.cpp
@@ -24,8 +24,9 @@ public:
   float A_star(std::pair<int,int>start, std::pair<int,int>finish) {
     int index_trenutniCvor = start.first*broj_kolona+start.second;
     int index_finishCvor = finish.first*broj_kolona+finish.second;
-    const int dx[8] = { 1, 0, -1,  0, 1, -1,  1, -1 };
-    const int dy[8] = { 0, -1,  0, 1, -1,  -1, 1, 1 };
+    const int dx[8] = { 0, -1, 0,  1, -1, -1,  1, 1 };
+    const int dy[8] = { 1, 0,  -1, 0, 1,  -1, 1, -1 };
+    //Desno, Gore, Lijevo, Dole, G. Desno, G. Lijevo, D. Desno, D. Lijevo
     std::vector<std::tuple<bool, float, int>>/*obradjen, g(n), od kojeg cvora je dobio */ obradjeniCvorovi(broj_redova*broj_kolona, std::make_tuple(false, 0.0f, 0));
 
     auto cmp = [](const std::tuple<int,float, float,int> &a, const std::tuple<int,float, float,int> &b){
@@ -49,14 +50,14 @@ public:
       int x = index_trenutniCvor / broj_kolona;
       int y = index_trenutniCvor % broj_kolona;
 
-      heuristika_za_okolne_cvorove[0] = Heuristika(x+1,y, finish.first, finish.second,1.);
-      heuristika_za_okolne_cvorove[1] = Heuristika(x,y-1, finish.first, finish.second,1.);
-      heuristika_za_okolne_cvorove[2] = Heuristika(x-1,y, finish.first, finish.second,1.);
-      heuristika_za_okolne_cvorove[3] = Heuristika(x,y+1, finish.first, finish.second,1.);
-      heuristika_za_okolne_cvorove[4] = Heuristika(x+1,y-1, finish.first, finish.second,1.);//Gore desno
+      heuristika_za_okolne_cvorove[0] = Heuristika(x,y+1, finish.first, finish.second,1.);//Desno
+      heuristika_za_okolne_cvorove[1] = Heuristika(x-1,y, finish.first, finish.second,1.);//Gore
+      heuristika_za_okolne_cvorove[2] = Heuristika(x,y-1, finish.first, finish.second,1.);//Lijevo
+      heuristika_za_okolne_cvorove[3] = Heuristika(x+1,y, finish.first, finish.second,1.);//Dole
+      heuristika_za_okolne_cvorove[4] = Heuristika(x-1,y+1, finish.first, finish.second,1.);//Gore desno
       heuristika_za_okolne_cvorove[5] = Heuristika(x-1,y-1, finish.first, finish.second,1.);//Gore lijevo
       heuristika_za_okolne_cvorove[6] = Heuristika(x+1,y+1, finish.first, finish.second,1.);//Dole desno
-      heuristika_za_okolne_cvorove[7] = Heuristika(x-1,y+1, finish.first, finish.second,1.);//Dole lijevo
+      heuristika_za_okolne_cvorove[7] = Heuristika(x+1,y-1, finish.first, finish.second,1.);//Dole lijevo
 
       for(int i = 0; i < 8; i++) {
         int nx = x + dx[i];
@@ -76,11 +77,17 @@ int main()
 
   std::vector<std::vector<float>> cvorovi(rows*cols, std::vector<float>(8, 1.0f));
 
+  cvorovi[21][0] = 100.0f;
+  cvorovi[21][1] = 150.0f;
+  cvorovi[21][2] = 150.0f;
+  cvorovi[21][3] = 150.0f;
+  cvorovi[21][4] = 150.0f;
+  cvorovi[21][5] = 150.0f;
   cvorovi[21][6] = 150.0f;
-  //cvorovi[11][4] = 3.0f;
+  cvorovi[21][7] = 150.0f;
 
   std::pair<int,int> start = {2,1};
-  std::pair<int,int> finish = {4,3};
+  std::pair<int,int> finish = {7,1};
 
   Graf graf(cvorovi, rows, cols);
   float path_cost = graf.A_star(start, finish);
